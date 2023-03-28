2e82e13f52c250c98d90d9fc151cc42e11cece40
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-03-28 23:23:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -387,7 +387,7 @@ async def cb_handler(Client, query: CallbackQuery):
             )
         else:
            lokeshkhushi = lokeshkhushi.find_one({"chat_id": query.message.chat.id})
-           if not is_candy:           
+           if notlokeshkhushi:           
                 await query.edit_message_text(f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.**")
             if lokeshkhushi:
                lokeshkhushi.delete_one({"chat_id": query.message.chat.id})
